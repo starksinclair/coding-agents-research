35f68160da03ee84a3237c9945df13bd729ab19a
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2026-07-07 21:25:47 -0400

    Implement canTraverseAllPairs function in solution.js to determine if all pairs in the input array can be traversed based on their prime factors. The function utilizes union-find to group indices and checks connectivity.

--- tasks/livecodebench/LCB-08/solution.js
+++ tasks/livecodebench/LCB-08/solution.js
@@ -4,7 +4,53 @@
  * @return {boolean}
  */
 function canTraverseAllPairs(nums) {
-  // Tool places solution here
+  if (nums.length === 1) return true;
+  if (nums.some((n) => n === 1)) return false;
+
+  const parent = new Map();
+
+  function find(x) {
+    if (!parent.has(x)) parent.set(x, x);
+    if (parent.get(x) !== x) parent.set(x, find(parent.get(x)));
+    return parent.get(x);
+  }
+
+  function union(a, b) {
+    const ra = find(a);
+    const rb = find(b);
+    if (ra !== rb) parent.set(ra, rb);
+  }
+
+  function primeFactors(n) {
+    const factors = new Set();
+    let d = 2;
+    while (d * d <= n) {
+      if (n % d === 0) {
+        factors.add(d);
+        while (n % d === 0) n = Math.floor(n / d);
+      }
+      d++;
+    }
+    if (n > 1) factors.add(n);
+    return factors;
+  }
+
+  const primeToIndex = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    for (const p of primeFactors(nums[i])) {
+      if (primeToIndex.has(p)) {
+        union(i, primeToIndex.get(p));
+      } else {
+        primeToIndex.set(p, i);
+      }
+    }
+  }
+
+  const root = find(0);
+  for (let i = 1; i < nums.length; i++) {
+    if (find(i) !== root) return false;
+  }
+  return true;
 }
 
 module.exports = { canTraverseAllPairs };
