e3c2fd9ec59efa8f39713b15286e1edcda864f91
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2026-07-07 21:11:57 -0400

    Implement continuousSubarrays function in solution.js to count the number of continuous subarrays where the difference between the maximum and minimum values is at most 2. Utilize two deques for efficient tracking of max and min values during iteration.

--- tasks/livecodebench/LCB-06/solution.js
+++ tasks/livecodebench/LCB-06/solution.js
@@ -4,7 +4,32 @@
  * @return {number}
  */
 function continuousSubarrays(nums) {
-  // Tool places solution here
+  const maxDeque = [];
+  const minDeque = [];
+  let left = 0;
+  let count = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    while (maxDeque.length && nums[maxDeque[maxDeque.length - 1]] <= nums[right]) {
+      maxDeque.pop();
+    }
+    maxDeque.push(right);
+
+    while (minDeque.length && nums[minDeque[minDeque.length - 1]] >= nums[right]) {
+      minDeque.pop();
+    }
+    minDeque.push(right);
+
+    while (nums[maxDeque[0]] - nums[minDeque[0]] > 2) {
+      left++;
+      if (maxDeque[0] < left) maxDeque.shift();
+      if (minDeque[0] < left) minDeque.shift();
+    }
+
+    count += right - left + 1;
+  }
+
+  return count;
 }
 
 module.exports = { continuousSubarrays };
